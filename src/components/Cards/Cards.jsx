3761370af3d59c996a85ef576c2f2686c494a1b2
Dieferson Soares <62387982+frontendDieferson@users.noreply.github.com>
2021-02-20 18:55:07 -0300

    Update Cards.jsx

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
+import CountUp from "react-countup";
+import cx from "classnames";
 
-import CardComponent from './Card/Card';
-import styles from './Cards.module.css';
-
-const Info = ({
+const Cards = ({
   data: { confirmed, recovered, deaths, lastUpdate },
   country,
 }) => {
@@ -13,41 +13,71 @@ const Info = ({
   }
   const active = confirmed["value"] - recovered["value"] - deaths["value"];
   let carddetails = [
-
+    {
+      style: styles.infected,
+      text: "Infectados",
+      value: confirmed.value,
+      bottomText: "Número de casos de infecção de COVID-19",
+    },
+    {
+      style: styles.recovered,
+      text: "Recuperados",
+      value: recovered.value,
+      bottomText: "Número de recuperações de COVID-19",
+    },
+    {
+      style: styles.deaths,
+      text: "Mortes",
+      value: deaths.value,
+      bottomText: "Número de mortes causadas por COVID-19",
+    },
+    {
+      style: styles.active,
+      text: "Ativos",
+      value: active,
+      bottomText: "Número de casos ativos de COVID-19",
+    },
+  ];
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <CardComponent
-          className={styles.infected}
-          cardTitle="Infectados"
-          value={confirmed.value}
-          lastUpdate={lastUpdate}
-          cardSubtitle="Número de casos ativos de COVID-19."
-        />
-        <CardComponent
-          className={styles.recovered}
-          cardTitle="Recuperados"
-          value={recovered.value}
-          lastUpdate={lastUpdate}
-          cardSubtitle="Número de recuperações do COVID-19. "
-        />
-        <CardComponent
-          className={styles.deaths}
-          cardTitle="Mortes"
-          value={deaths.value}
-          lastUpdate={lastUpdate}
-          cardSubtitle="Número de mortes causadas por COVID-19."
-        />
-        <CardComponent
-          className={styles.active}
-          cardTitle="Ativos"
-          value={active.value}
-          lastUpdate={lastUpdate}
-          cardSubtitle="Número de casos ativos de COVID-19."
-        />
+        {carddetails.map((detail, index) => (
+          <Grid
+            item
+            component={Card}
+            xs={12}
+            md={2}
+            className={cx(styles.Card, detail.style)}
+            key={index}
+            style={{ margin: "0px 23.675px", padding: "12px" }}
+          >
+            <CardContent>
+              <Typography color="textPrimary" gutterBottom>
+                <b>{detail.text}</b>
+              </Typography>
+              <Typography variant="h5">
+                <CountUp
+                  start={0}
+                  end={detail.value}
+                  duration={2}
+                  separator=","
+                />
+              </Typography>
+              <Typography color="textPrimary">última atualização: </Typography>
+              <Typography color="textSecondary" variant="body2">
+                {new Date(lastUpdate).toDateString()}
+              </Typography>
+              <Typography color="textSecondary" variant="body2">
+                {new Date(lastUpdate).toLocaleTimeString()}
+              </Typography>
+              <Typography variant="body2">{detail.bottomText}</Typography>
+              <Typography color="textPrimary"> {country} </Typography>
+            </CardContent>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
 };
 
-export default Info;
+export default Cards;
